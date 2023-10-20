4d7463c80bc771fe07ebde0b608c3a4a4360e007
Karthik Vadhiraj <karthik.vadhiraj@ccmllc.com>
2023-10-20 14:32:56 -0500

    slight changes per react18 createRoot

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const Person = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("p", {}, props.occupation),
+  return React.createElement("div", {key:'per'}, [
+    React.createElement("h1", {key:1}, props.name),
+    React.createElement("p", {key:2}, props.occupation),
   ]);
 };
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { className: "title" }, "React Is Rendered"),
+  return React.createElement("div", {key:'app'}, [
+    React.createElement("h1", { className: "title", key:'title' }, "React Is Rendered"),
     React.createElement(Person, {
       name: "Krishna Devaraya",
       occupation: "King",
+      key:'p1',
     }),
     React.createElement(Person, {
       name: "Tenali Ramakrishna",
       occupation: "Wise Minister",
+      key:'p2',
     }),
   ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
